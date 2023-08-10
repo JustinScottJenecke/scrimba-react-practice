5e3d3592af89b888a5a06513334ce3a5775a1fa0
Justin <justinjenecke@gmail.com>
2023-08-10 09:24:43 +0200

    import dummy data and rendering data in jsx

--- udemy/02-react-refresh/meetup-clone/src/pages/Meetups.js
+++ udemy/02-react-refresh/meetup-clone/src/pages/Meetups.js
@@ -1,6 +1,19 @@
+import { MEETUPS } from '../data/dummy'
+
 const MeetupsPage = () => {
     return (
-        <h1 className="title">All Meetups</h1>
+        <section>
+            <h1 className="title">All Meetups</h1>
+            <ul>
+                {
+                    MEETUPS.map( (meetup) => {
+                        return (
+                            <li key={meetup.id}>{meetup.title}</li>
+                        )
+                    })
+                }
+            </ul>
+        </section>
     )
 }
 
