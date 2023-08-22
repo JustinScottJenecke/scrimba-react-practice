6a3fe90822c6d406c0897b8f7c05f7b752bb8c4f
Justin <justinjenecke@gmail.com>
2023-08-22 14:39:34 +0200

    creates empty component files for static site

--- projects/1-react-website/index.js
+++ projects/1-react-website/index.js
@@ -1,12 +1,12 @@
-const navbar = (
-    <nav>
-        <h1>Bob's Bistro</h1>
-        <ul>
-            <li>Menu</li>
-            <li>About</li>
-            <li>Contact</li>
-        </ul>
-    </nav>
+
+
+const app = (
+    <main>
+        
+    </main>
 )
 
-ReactDOM.render(navbar, document.getElementById("root"))
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(navbar)
+
+ReactDOM.render(app, document.getElementById("root"))
